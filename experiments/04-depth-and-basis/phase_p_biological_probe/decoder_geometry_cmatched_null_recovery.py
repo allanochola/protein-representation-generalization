a32c2970eaab5d75ba5ab5cde510495541431a29
Allan Ochola <allanochola4@gmail.com>
2026-09-09 04:14:46 +0000

    Authorize C-matched null recovery

--- experiments/04-depth-and-basis/phase_p_biological_probe/decoder_geometry_cmatched_null_recovery.py
+++ experiments/04-depth-and-basis/phase_p_biological_probe/decoder_geometry_cmatched_null_recovery.py
@@ -61,7 +61,7 @@ import tempfile
 # HARD EXECUTION GATE
 # ---------------------------------------------------------------------------
 
-ENABLE_CMATCHED_RECOVERY = False
+ENABLE_CMATCHED_RECOVERY = True
 
 
 # ---------------------------------------------------------------------------
