be46f1ba0fa1231880d0d3970fee5b95aaffd1c6
Allan Ochola <allanochola4@gmail.com>
2026-09-09 06:01:44 +0000

    Re-close obsolete decoder geometry aggregation driver

--- experiments/04-depth-and-basis/phase_p_biological_probe/decoder_geometry_aggregation.py
+++ experiments/04-depth-and-basis/phase_p_biological_probe/decoder_geometry_aggregation.py
@@ -37,7 +37,7 @@ import numpy as np
 # HARD EXECUTION GATE
 # ---------------------------------------------------------------------------
 
-ENABLE_AGGREGATION = True
+ENABLE_AGGREGATION = False
 
 
 # ---------------------------------------------------------------------------
